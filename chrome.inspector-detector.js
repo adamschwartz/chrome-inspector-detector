89a812e099fadabe6b09f5bb6180939a948ba76f
Adam Schwartz <aschwartz@hubspot.com>
2013-03-23 20:34:05 -0400

    Adding some clarifying comments

--- chrome.inspector-detector.js
+++ chrome.inspector-detector.js
@@ -6,15 +6,22 @@
 
     window.chrome.inspector = window.chrome.inspector || {};
 
+    // Mar 23 2013
     // Detects if the Chrome Inspector is open
     // http://stackoverflow.com/a/15567735/131898
     window.chrome.inspector.isOpen = function () {
         var existingProfiles = console.profiles.length;
         console.profile();
         console.profileEnd();
+
+        // Mar 23 2013
+        // Note that this has no effect when the inspector
+        // setting "Preserve Log upon navigation" is true
+        // http://web.archiveorange.com/archive/v/fwvdeLnVHqVyTY2UZiaB
         if (console.clear) {
             console.clear();
         }
+
         return console.profiles.length > existingProfiles;
     };
 
