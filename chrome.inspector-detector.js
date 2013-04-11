195fbb9633525403a5cf9c298d917f2491ff5cf4
Zack Bloom <zackbloom@gmail.com>
2013-04-11 00:10:50 -0400

    Get rid of test config

--- chrome.inspector-detector.js
+++ chrome.inspector-detector.js
@@ -55,46 +55,26 @@
        origGroupEnd.apply(console);
     };
 
-    window.chrome.inspector.tests = {
+    var isOpen = function() {
+        // Try running a profile to see if it's open
+        // http://stackoverflow.com/a/15567735/131898
+        var existingProfiles = console.profiles.length;
 
-        open: {
-            profile: function(){
-                // Try running a profile to see if it's open
-                // http://stackoverflow.com/a/15567735/131898
-                var existingProfiles = console.profiles.length;
-
-                if (!groupActive) {
-                    groupActive = true;
-                    origGroupCollapsed.apply(console);
-                }
-
-                console.profile('Inspector Detector');
-                console.profileEnd();
-
-                if (console.profiles.length > existingProfiles) {
-                    return true;
-                }
-
-                return false;
-            }
-        },
-
-        docked: {
-            height: function(){
-                var zoom = document.width / (document.body.clientWidth + parseInt(getComputedStyle(document.body)['margin-left'], 10) + parseInt(getComputedStyle(document.body)['margin-left'], 10));
-
-                // Try detecting by comparing the inner and outer window sizes
-                if (window.outerHeight > 1 + Math.ceil((zoom * window.innerHeight) + window.chrome.inspector._windowHeightOffset) ||
-                    window.outerWidth > 1 + Math.ceil(zoom * window.innerWidth)) {
-
-                    return true;
-                }
-
-                return false;
-            }
+        if (!groupActive) {
+            groupActive = true;
+            origGroupCollapsed.apply(console);
         }
-    };
 
+        console.profile('Inspector Detector');
+        console.profileEnd();
+
+        if (console.profiles.length > existingProfiles) {
+            return true;
+        }
+
+        return false;
+    };
+    
     // Account for the height of the omnibar and bookmarks bar
     // Can be overridden by setting window.chrome.inspector._windowHeightOffset yourself
     // http://stackoverflow.com/a/7530254/131898
@@ -103,44 +83,25 @@
     if (window.chrome.inspector._windowHeightOffset === undefined)
         window.chrome.inspector._windowHeightOffset = (window.chrome.inspector.tests.open.profile() ? 200 : window.outerHeight - window.innerHeight);
 
-    var getTests = function (spec){
-        var tests = {}, testType, testName, test;
+    var isDocked = function() {
+        var zoom = document.width / (document.body.clientWidth + parseInt(getComputedStyle(document.body)['margin-left'], 10) + parseInt(getComputedStyle(document.body)['margin-left'], 10));
 
-        for (testType in spec) {
-            // The tests can be specified as keys in the `window.chrome.inspector.tests[testType]` object,
-            // or as functions.  Specify false (or don't include the key) to not run that
-            // class of test.
-            testName = spec[testType];
-            if (typeof testName == 'string')
-                test = window.chrome.inspector.tests[testType][testName];
-            else if (typeof testName == 'function')
-                test = testName;
-            else
-                continue;
+        // Try detecting by comparing the inner and outer window sizes
+        if (window.outerHeight > 1 + Math.ceil((zoom * window.innerHeight) + window.chrome.inspector._windowHeightOffset) ||
+            window.outerWidth > 1 + Math.ceil(zoom * window.innerWidth)) {
 
-            tests[testType] = test;
+            return true;
         }
 
-        return tests;
+        return false;
     };
 
-    window.chrome.inspector.detector = function (options) {
-        var state, tests;
-
-        options = options || window.chrome.inspector.options || {};
-
-        // `options.tests` should be an object mapping a state to be looked for to
-        // a specific test to execute to test for it.
-        options.tests = options.tests || {
-            open: 'profile',
-            docked: 'height'
-        };
-
-        tests = getTests(options.tests);
+    window.chrome.inspector.detector = function () {
+        var state;
 
         state = {};
-        state.open = !!(tests.open && tests.open());
-        state.docked = !!(state.open && tests.docked && tests.docked());
+        state.open = isOpen() 
+        state.docked = state.open && isDocked();
 
         return state;
     };
